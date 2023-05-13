f73dd9906e786ca3cfb73714f6108da4cfe53bdc
shemaabra <abrashema9@gmail.com>
2023-05-13 15:44:02 +0300

    finalizing

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -1,4 +1,6 @@
 import 'package:booktickets/utils/app_layout.dart';
+import 'package:booktickets/widgets/column_layout.dart';
+import 'package:booktickets/widgets/layout_builder_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:booktickets/utils/app_styles.dart';
 import 'package:gap/gap.dart';
@@ -80,7 +82,7 @@ class ProfileScreen extends StatelessWidget {
                         Text(
                           "Premium Status",
                           style: GoogleFonts.montserrat(
-                              textStyle: TextStyle(
+                              textStyle: const TextStyle(
                             fontWeight: FontWeight.w600,
                           )),
                         ),
@@ -100,7 +102,7 @@ class ProfileScreen extends StatelessWidget {
                       style: GoogleFonts.montserrat(
                         textStyle: Styles.textStyle.copyWith(
                           color: Styles.primaryColor,
-                          fontWeight: FontWeight.w300,
+                          fontWeight: FontWeight.w500,
                         ),
                       ),
                     ),
@@ -110,9 +112,8 @@ class ProfileScreen extends StatelessWidget {
             ],
           ),
           Gap(AppLayout.getHeight(8)),
-          Divider(
-            color: Colors.grey.shade300,
-          ),
+          Divider(color: Colors.grey.shade300),
+          Gap(AppLayout.getHeight(8)),
           Stack(
             children: [
               Container(
@@ -125,14 +126,209 @@ class ProfileScreen extends StatelessWidget {
                   ),
                 ),
               ),
-              Container(
-                padding: EdgeInsets.all(AppLayout.getHeight(30)),
-                decoration: BoxDecoration(
+              Positioned(
+                right: -45,
+                top: -40,
+                child: Container(
+                  padding: EdgeInsets.all(AppLayout.getHeight(30)),
+                  decoration: BoxDecoration(
+                    color: Colors.transparent,
                     shape: BoxShape.circle,
-                    border: Border.all(width: 18, color: const Color(0xFF264CD2),),),
+                    border: Border.all(
+                      width: 18,
+                      color: const Color(0xFF264CD2),
+                    ),
+                  ),
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.symmetric(
+                  horizontal: AppLayout.getHeight(25),
+                  vertical: AppLayout.getHeight(15),
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    CircleAvatar(
+                      maxRadius: 25,
+                      backgroundColor: Colors.white,
+                      child: Icon(
+                        FluentSystemIcons.ic_fluent_lightbulb_filament_filled,
+                        color: Styles.primaryColor,
+                        size: 27,
+                      ),
+                    ),
+                    Gap(AppLayout.getHeight(12)),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "You\'ve got new reward",
+                          style: GoogleFonts.montserrat(
+                            textStyle: Styles.headLineStyle2.copyWith(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 18,
+                            ),
+                          ),
+                        ),
+                        Text(
+                          "You have 96 flight this year",
+                          style: GoogleFonts.montserrat(
+                            textStyle: Styles.headLineStyle1.copyWith(
+                              color: Colors.white.withOpacity(0.9),
+                              fontWeight: FontWeight.w500,
+                              fontSize: 14,
+                            ),
+                          ),
+                        )
+                      ],
+                    )
+                  ],
+                ),
               ),
             ],
           ),
+          Gap(AppLayout.getHeight(25)),
+          Text(
+            "Accumulated Miles",
+            style: GoogleFonts.montserrat(textStyle: Styles.headLineStyle2),
+          ),
+          Gap(AppLayout.getHeight(20)),
+          Container(
+            padding: EdgeInsets.symmetric(horizontal: AppLayout.getHeight(15)),
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(
+                  AppLayout.getHeight(18),
+                ),
+                color: Styles.bgColor,
+                boxShadow: [
+                  BoxShadow(
+                      color: Colors.grey.shade200,
+                      blurRadius: 1,
+                      spreadRadius: 1),
+                ]),
+            child: Column(
+              children: [
+                Gap(AppLayout.getHeight(15)),
+                Text(
+                  "192802",
+                  style: GoogleFonts.montserrat(
+                    textStyle: TextStyle(
+                      fontSize: 45,
+                      color: Styles.textColor,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                Gap(AppLayout.getHeight(20)),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      "Miles Accrued",
+                      style: GoogleFonts.montserrat(
+                        textStyle: Styles.headLineStyle4.copyWith(
+                          fontSize: 16,
+                        ),
+                      ),
+                    ),
+                    Text(
+                      "13 May 2023",
+                      style: GoogleFonts.montserrat(
+                        textStyle: Styles.headLineStyle4.copyWith(
+                          fontSize: 16,
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+                Gap(AppLayout.getHeight(5)),
+                Divider(color: Colors.grey.shade300),
+                Gap(AppLayout.getHeight(5)),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: const [
+                    AppColumnLayout(
+                        firstText: "23 042",
+                        secondText: "Miles",
+                        alignment: CrossAxisAlignment.start,
+                        isColor: true),
+                    AppColumnLayout(
+                        firstText: "Airline Co",
+                        secondText: "Received from",
+                        alignment: CrossAxisAlignment.end,
+                        isColor: true)
+                  ],
+                ),
+                Gap(AppLayout.getHeight(15)),
+                const AppLayoutBuilderWidget(
+                  section: 12,
+                  width: 3,
+                  isColor: true,
+                ),
+                Gap(AppLayout.getHeight(15)),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: const [
+                    AppColumnLayout(
+                      firstText: "24",
+                      secondText: "Miles",
+                      alignment: CrossAxisAlignment.start,
+                      isColor: true,
+                    ),
+                    AppColumnLayout(
+                      firstText: "Mc Donal\'s",
+                      secondText: "Received from",
+                      alignment: CrossAxisAlignment.end,
+                      isColor: true,
+                    ),
+                  ],
+                ),
+                Gap(AppLayout.getHeight(15)),
+                const AppLayoutBuilderWidget(
+                  section: 12,
+                  width: 3,
+                  isColor: true,
+                ),
+                Gap(AppLayout.getHeight(15)),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: const [
+                    AppColumnLayout(
+                      firstText: "52 349",
+                      secondText: "Miles",
+                      alignment: CrossAxisAlignment.start,
+                      isColor: true,
+                    ),
+                    AppColumnLayout(
+                      firstText: "Exuma",
+                      secondText: "Received from",
+                      alignment: CrossAxisAlignment.end,
+                      isColor: true,
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+          Gap(AppLayout.getHeight(60)),
+          InkWell(
+            onTap: ()=>{debugPrint('tapped more miles')},
+            child: Center(
+              child: Text(
+                "How to get more miles",
+                style: GoogleFonts.montserrat(
+                  textStyle: Styles.textStyle.copyWith(
+                    color: Styles.primaryColor,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+              ),
+            ),
+          )
         ],
       ),
     );
